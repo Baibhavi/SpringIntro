d03a87b082677e83243f34fd2b756db0ff0d6665
Baibhavi <baibhavipandey@gmail.com>
2025-03-01 20:05:30 +0530

    Added: Use Case-5 for SpringBoot Intro with PutMapping

--- springintro/src/main/java/com/springintro/controller/HelloController.java
+++ springintro/src/main/java/com/springintro/controller/HelloController.java
@@ -6,26 +6,37 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/home")
 public class HelloController {
+    //localhost:8080/home
     @GetMapping(value = {"", "/", "/home"})
     public String restController(){
         return "Hello from BridgeLabz";
     }
 
     // used request mapping to get the query in the form of name then it will be displayed
+    //localhost:8080/home/query
     @RequestMapping(value = "/query")
     public String queryController(@RequestParam(value = "name") String name){
         return "Hello " + name + " !";
     }
 
     //used params to take input and display it .
+    //localhost:8080/home/param/Baibhavi
     @GetMapping("/param/{name}")
     public String paramController(@PathVariable(value = "name") String name){
         return "Hello " + name + " !";
     }
 
     // displaying users first name and last name with RequestBody and PostMapping
+    //localhost:8080/home/post
     @PostMapping("/post")
     public String postController(@RequestBody User user){
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " !";
+    }
+
+    // displaying users first name and last name with PutMapping
+    //    http://localhost:8080/home/put/Baibhavi?LastName=Pandey
+    @PutMapping("/put/{firstName}")
+    public String putController(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName){
+        return "Hello " + firstName + " " + lastName + " !";
     }
 }
